4e594048db832dbfbe2f8cb916d75f50a3795951
Wirawat Lianudom <sayhi@365zocial.com>
2026-02-15 21:30:36 +0700

    Comment out EDC display in closing summary (credit card already shown)

--- app/dashboard/staff/closings/new/page.tsx
+++ app/dashboard/staff/closings/new/page.tsx
@@ -1039,7 +1039,8 @@ export default function NewClosingPage() {
                         })}
                       </td>
                     </tr>
-                    <tr>
+                    {/* Comment: ยอด EDC - ไม่แสดงตอนนี้เพราะมีค่าบัตรเครดิตแยกแล้ว */}
+                    {/* <tr>
                       <td className="px-4 py-3 text-sm text-gray-900">
                         ยอด EDC
                       </td>
@@ -1048,7 +1049,7 @@ export default function NewClosingPage() {
                           minimumFractionDigits: 2,
                         })}
                       </td>
-                    </tr>
+                    </tr> */}
                     <tr>
                       <td className="px-4 py-3 text-sm text-gray-900">
                         นับเงินสด (ลายมือ)
